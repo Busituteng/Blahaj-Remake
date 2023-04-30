1a6590d5b97dc965064205dad918ccfe214704e3
BunnimimiTotem <xcxiaocang@outlook.com>
2023-04-30 11:49:58 +0800

    Add files via upload

---
+++ src/main/java/com/hibi/blahaj/item/ItemBlueShark.java
@@ -0,0 +1,60 @@
+
+package com.hibi.blahaj.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import com.hibi.blahaj.ElementsBlahajremakeMod;
+
+@ElementsBlahajremakeMod.ModElement.Tag
+public class ItemBlueShark extends ElementsBlahajremakeMod.ModElement {
+	@GameRegistry.ObjectHolder("blahajremake:blue_shark")
+	public static final Item block = null;
+	public ItemBlueShark(ElementsBlahajremakeMod instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("blahajremake:blue_shark", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 1;
+			setUnlocalizedName("blue_shark");
+			setRegistryName("blue_shark");
+			setCreativeTab(CreativeTabs.MISC);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+	}
+}
---
+++ src/main/java/com/hibi/blahaj/item/ItemGrayShark.java
@@ -0,0 +1,60 @@
+
+package com.hibi.blahaj.item;
+
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.block.state.IBlockState;
+
+import com.hibi.blahaj.ElementsBlahajremakeMod;
+
+@ElementsBlahajremakeMod.ModElement.Tag
+public class ItemGrayShark extends ElementsBlahajremakeMod.ModElement {
+	@GameRegistry.ObjectHolder("blahajremake:gray_shark")
+	public static final Item block = null;
+	public ItemGrayShark(ElementsBlahajremakeMod instance) {
+		super(instance, 4);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void registerModels(ModelRegistryEvent event) {
+		ModelLoader.setCustomModelResourceLocation(block, 0, new ModelResourceLocation("blahajremake:gray_shark", "inventory"));
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			setMaxDamage(0);
+			maxStackSize = 1;
+			setUnlocalizedName("gray_shark");
+			setRegistryName("gray_shark");
+			setCreativeTab(CreativeTabs.MISC);
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getMaxItemUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, IBlockState par2Block) {
+			return 1F;
+		}
+	}
+}
